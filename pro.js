61a647defd3025629c943a64051d17ad072ee9a1
Neel Patel <neel.patel@metropolia.fi>
2021-10-18 11:28:46 +0300

    BMI-Calculator
    - Calculate height and Weight for BMI Index
    - Calculate BMI Index range
    - Print BMI Index range

---
+++ pro.js
@@ -0,0 +1,30 @@
+function calculate()
+{
+    var height = parseInt(document.getElementById("heightInputId").value);
+    var width = parseInt(document.getElementById("weightInputId").value);
+    var y = converter(height);
+    var calculation = width / (y*y);
+    document.getElementById("result").innerHTML = calculation;
+    overviewResult(calculation);
+
+}
+function converter(x)
+{
+   var a = x/100;
+   return a;
+}
+
+function overviewResult(calculation)
+{
+    if (calculation <= 18.6) {
+            document.getElementById("print").innerHTML = "Under Weight (Less than 18.6)";
+    }
+    else if(calculation > 18.6 && calculation <= 24.9 ){
+            document.getElementById("print").innerHTML = "Normal Range (18.6 and 24.6)";
+    }
+     else {
+        document.getElementById("print").innerHTML = "Over Weight (greater than 24.9)";
+    }
+}
+
+
